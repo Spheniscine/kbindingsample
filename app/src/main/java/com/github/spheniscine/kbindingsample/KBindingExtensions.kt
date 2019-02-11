a0aff584b836ca90bcc32e6ba39dfc10d09174b6
spheniscine <spheniscine@gmail.com>
2019-02-11 22:35:22 +0800

    Add platform type disambiguation

--- app/src/main/java/com/github/spheniscine/kbindingsample/KBindingExtensions.kt
+++ app/src/main/java/com/github/spheniscine/kbindingsample/KBindingExtensions.kt
@@ -3,7 +3,7 @@ package com.github.spheniscine.kbindingsample
 import com.github.spheniscine.kbinding.bindingdefs.K2WayBinder
 import com.rilixtech.CountryCodePicker
 
-val CountryCodePicker.iso2way get() = K2WayBinder(
+val CountryCodePicker.iso2way: K2WayBinder<String> get() = K2WayBinder(
     get = ::getSelectedCountryNameCode,
     set = ::setCountryForNameCode,
     attachListener = { setOnCountryChangeListener { it() } }
