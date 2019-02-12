d5d5e9dab22e2049ae2cae058ba99d7cc2e88831
spheniscine <spheniscine@gmail.com>
2019-02-12 17:01:54 +0800

    Upgrade kotlin version, remove unused function

--- kbinding/src/main/java/com/github/spheniscine/kbinding/Box.kt
+++ kbinding/src/main/java/com/github/spheniscine/kbinding/Box.kt
@@ -1,7 +1,4 @@
 package com.github.spheniscine.kbinding
 
 
-data class Box<out T>(val value: T) {
-    /** returns a new [Box] whose value is modified by the [transform] function */
-    inline fun <N> map(transform: (T) -> N) = Box(transform(value))
-}
+data class Box<out T>(val value: T)
