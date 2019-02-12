0c0892bc84b7821f8d41bb5ffcd33e63fa1aff40
spheniscine <spheniscine@gmail.com>
2019-02-12 12:29:53 +0800

    work on KBindableArray

--- kbinding/src/main/java/com/github/spheniscine/kbinding/KBindableArray.kt
+++ kbinding/src/main/java/com/github/spheniscine/kbinding/KBindableArray.kt
@@ -1,25 +1,33 @@
 package com.github.spheniscine.kbinding
 
-interface KBindableValArray<T> {
+interface KBindableValArray<T>: Collection<T> {
     val kbvals : List<KBindableVal<T>>
 
-    operator fun get(index: Int) : T
+    operator fun get(index: Int) : T = kbvals[index].value
 
-    fun toList() : List<T>
+    fun toList() : List<T> = kbvals.map { it.value }
+
+    override val size get() = kbvals.size
+    override fun isEmpty() = kbvals.isEmpty()
+    override fun contains(element: T) = toList().contains(element)
+    override fun iterator(): Iterator<T> = toList().iterator()
+    override fun containsAll(elements: Collection<T>) = toList().containsAll(elements)
 }
 
 interface KBindableVarArray<T>: KBindableValArray<T> {
     val kbvars : List<KBindableVar<T>>
-
-    operator fun set(index: Int, value: T)
-}
-
-abstract class KBindableVarArrayImpl<T>: KBindableVarArray<T> {
     override val kbvals: List<KBindableVal<T>> get() = kbvars
 
-    override fun get(index: Int): T = kbvars[index].value
+    operator fun set(index: Int, value: T) { kbvars[index].value = value }
 
-    override fun set(index: Int, value: T) { kbvars[index].value = value }
+    companion object {
+        operator fun <T> invoke(kbvars: List<KBindableVar<T>>) =
+            object : KBindableVarArray<T> {
+                override val kbvars = kbvars
+            }
 
-    override fun toList(): List<T> = kbvars.map { it.value }
+        inline operator fun <T> invoke(size: Int, init: (Int) -> T) =
+            invoke(List(size) { KBindableVar(init(it)) })
+    }
 }
+
